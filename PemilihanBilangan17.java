b573b0e14d3dab9a814430beccbea5139bc2ba57
sitiaisyah09 <aisyah62881@gmail.com>
2024-09-24 16:35:37 +0700

    TernaryOperator

--- PemilihanBilangan17.java
+++ PemilihanBilangan17.java
@@ -7,8 +7,14 @@ public class PemilihanBilangan17 {
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
-        String hasil = (angka % 2 == 0) ? "genap" : "ganjil";
-        System.out.println("Angka " + angka + " termasuk bilangan " + hasil);
+        if (angka % 2 == 0)
+        {
+            System.out.println("Angka " + angka + " termasuk bilangan genap" );
+        }
+        else
+        {
+            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+        }
         
     }
     
